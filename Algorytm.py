c49fbc4c2a4c79a905e74cc45a59cae74dc0f074
piotrini <p1terrr40@gmail.com>
2024-11-11 14:49:20 +0100

    Added RangeQuery

--- Algorytm.py
+++ Algorytm.py
@@ -1,3 +1,22 @@
 import numpy as np
-data = np.random.rand(20, 2) * 10
-print(data)
+
+
+
+
+def RangeQuery(DB, distFunc, Q, eps):
+    Point_Neighbors = []
+    for point in DB:
+        if distFunc(Q, point) <= eps:
+            Point_Neighbors.append(point)
+
+    return Point_Neighbors
+
+def DBSCAN_Algorithm(DB, distFunc, eps, minPts):
+    Cluster_Counter = 0
+
+
+DB = np.random.rand(20, 2) * 10
+print(DB)
+
+
+
